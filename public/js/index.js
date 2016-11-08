d081617b922b67779c385a5a1acd42d8e88b0d54
ryan.veitch <ryan.r.veitch@gmail.com>
2016-11-08 17:30:17 -0600

    updated index

--- public/js/index.js
+++ public/js/index.js
@@ -4,9 +4,14 @@ app.controller('ctrl', function ($scope, $http) {
 
 	$http.get("/results")
     .then(function(response) {
-        $scope.cards = response.data.contests.us.president.candidates;
+				$scope.results = response.data;
 				$scope.updated = response.data.updated;
-				console.log(response.data.contests.us.president.candidates);
+				$scope.precincts = response.data.precincts;
+				$scope.ballots = response.data.ballots;
+				$scope.contests = response.data.contests.state.nd;
+				$scope.cards = response.data.contests.us.president.candidates;
+				//console.log(response.data.contests.us.president.candidates);
+				console.log(response.data)
     });
 
 });
