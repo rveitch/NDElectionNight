516099070a72386169f3c03e6a9b3487dff164ed
ryan.veitch <ryan.r.veitch@gmail.com>
2016-11-08 01:42:49 -0600

    connected front end

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var fs = require('fs');
 if(typeof require !== 'undefined') XLS = require('xlsjs');
-var schema = JSON.parse(fs.readFileSync('schema.json'));
+var schema = JSON.parse(fs.readFileSync('./schema/schema.json'));
 //var express = require('express');
 //var app = express();
 
@@ -9,7 +9,7 @@ var schema = JSON.parse(fs.readFileSync('schema.json'));
 JSONconvert();
 
 function XLSRequest() {
-  var localFile = fs.createWriteStream('results.xls');
+  var localFile = fs.createWriteStream('./results/results.xls');
 
   request
     .get('http://results.sos.nd.gov/ResultsExport.aspx?cat=SWALL&type=SW&text=Race&area=')
@@ -24,7 +24,7 @@ function XLSRequest() {
 function JSONconvert() {
 	//var sheets = workbook.SheetNames;
 	//var Sheet1A1 = workbook.Sheets[sheets[0]]['A1'].v;
-	var workbook = XLS.readFile('./results.xls');
+	var workbook = XLS.readFile('./results/results.xls');
 	var results = workbook.Sheets.Results;
 	//console.log(results['A1'].v);
 
@@ -34,13 +34,15 @@ function JSONconvert() {
 	schema.Updated = results['A4'].v; // TODO: parse Updated
 
 	// PRESIDENT
-	schema.Contests.US.President.Candidates[0].Votes = results['B7'].v; // write-in
-	schema.Contests.US.President.Candidates[1].Votes = results['C7'].v; // CLINTON
-	schema.Contests.US.President.Candidates[2].Votes = results['D7'].v; // TRUMP
-	schema.Contests.US.President.Candidates[3].Votes = results['E7'].v; // JOHNSON
-	schema.Contests.US.President.Candidates[4].Votes = results['F7'].v; // STEIN
-	schema.Contests.US.President.Candidates[5].Votes = results['G7'].v; // DE LA FUENTE
-	schema.Contests.US.President.Candidates[6].Votes = results['H7'].v; // CASTLE
+	schema.contests.us.president.candidates[0].votes = results['C7'].v; // CLINTON
+	schema.contests.us.president.candidates[1].votes = results['D7'].v; // TRUMP
+	schema.contests.us.president.candidates[2].votes = results['E7'].v; // JOHNSON
+	schema.contests.us.president.candidates[3].votes = results['F7'].v; // STEIN
+	schema.contests.us.president.candidates[4].votes = results['H7'].v; // CASTLE
+	schema.contests.us.president.candidates[5].votes = results['G7'].v; // DE LA FUENTE
+	schema.contests.us.president.candidates[6].votes = results['B7'].v; // write-in
 
-	console.log(schema.Contests.US.President);
+	console.log(schema.contests.us.president);
+	var json = require('./schema/schema.json'); //(with path)
+	console.log(json);
 }
--- server.js
+++ server.js
@@ -4,22 +4,17 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
-var RESULTS_FILE = path.join(__dirname, './results/all.json');
+if(typeof require !== 'undefined') XLS = require('xlsjs');
+var schema = JSON.parse(fs.readFileSync('./schema/schema.json'));
+var RESULTS_FILE = path.join(__dirname, './results/results.json');
 
 app.set('port', (process.env.PORT || 3000));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-/******************************************************************************/
-
-// Additional middleware which will set headers that we need on each request.
 app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
     res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Disable caching so we'll always get the latest results.
     res.setHeader('Cache-Control', 'no-cache');
     next();
 });
@@ -34,8 +29,50 @@ app.get('/results', function(req, res) {
   });
 });
 
+
 /****************** LISTEN *********************/
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
+	clearInterval(interval);
+	var interval = setInterval(function() {
+		XLSRequest(); // fetch every 5 minutes
+	}, 300000);
 });
+
+function XLSRequest() {
+  var localFile = fs.createWriteStream('./results/results.xls');
+
+  request
+    .get('http://results.sos.nd.gov/ResultsExport.aspx?cat=SWALL&type=SW&text=Race&area=')
+    .pipe(localFile)
+    .on('close', JSONconvert);
+
+		//JSONconvert();
+}
+
+function JSONconvert() {
+	var workbook = XLS.readFile('./results/results.xls');
+	var results = workbook.Sheets.Results;
+
+	schema.reporting = results['A3'].v;
+	// TODO: parse PRECINCTS
+	// TODO: parse Ballots
+	var updated = results['A4'].v.replace('Downloaded at ','');
+	console.log(updated);
+	schema.updated = results['A4'].v.replace('Downloaded at ',''); // TODO: parse Updated
+
+	// PRESIDENT
+	schema.contests.us.president.candidates[0].votes = results['C7'].v; // CLINTON
+	schema.contests.us.president.candidates[1].votes = results['D7'].v; // TRUMP
+	schema.contests.us.president.candidates[2].votes = results['E7'].v; // JOHNSON
+	schema.contests.us.president.candidates[3].votes = results['F7'].v; // STEIN
+	schema.contests.us.president.candidates[4].votes = results['H7'].v; // CASTLE
+	schema.contests.us.president.candidates[5].votes = results['G7'].v; // DE LA FUENTE
+	schema.contests.us.president.candidates[6].votes = results['B7'].v; // write-in
+
+	var wstream = fs.createWriteStream('results/results.json');
+	wstream.write(JSON.stringify(schema));
+	wstream.end();
+	console.log('results.json Updated');
+}
