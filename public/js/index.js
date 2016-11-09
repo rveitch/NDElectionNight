1de8f59172d87209d4d1073acb15f6211c1b8b89
ryan.veitch <ryan.r.veitch@gmail.com>
2016-11-08 20:55:28 -0600

    added ballots cast and reporting percents

--- public/js/index.js
+++ public/js/index.js
@@ -10,6 +10,8 @@ app.controller('ctrl', function ($scope, $http) {
 				$scope.ballots = response.data.ballots;
 				$scope.contests = response.data.contests.state.nd;
 				$scope.cards = response.data.contests.us.president.candidates;
+				$scope.reportingPercent = (json.precincts.reporting / json.precincts.total * 100).toFixed(2) + '%';
+				$scope.ballotsPercent = (json.ballots.cast / 570955 * 100).toFixed(2) + '%';
 				//console.log(response.data.contests.us.president.candidates);
 				console.log(response.data)
     });
